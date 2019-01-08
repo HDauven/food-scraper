f01f5748478f2a15d6edbe8a5d2b62e0e4bbd5bc
Hein Dauven <heindauven@gmail.com>
2019-01-08 21:25:53 +0100

    Update recipe model to store ratings and reviews as numbers.

--- models/recipe.js
+++ models/recipe.js
@@ -1,13 +1,26 @@
 const mongoose = rquire("mongoose");
 
 const recipeSchema = new mongoose.Schema({
-  recipeId: { type: Number, required: true },
-  name: { type: String, required: true },
-  categories: { type: [String], index: true },
+  recipeId: {
+    type: Number,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  categories: {
+    type: [String],
+    index: true
+  },
   recipeBy: String,
   imageUrl: String,
-  rating: String,
-  time: { prep: String, cook: String, readyIn: String },
+  rating: Number,
+  time: {
+    prep: String,
+    cook: String,
+    readyIn: String
+  },
   servings: String,
   ingredients: [String],
   instructions: [String],
@@ -19,10 +32,14 @@ const recipeSchema = new mongoose.Schema({
     cholesterol: String,
     sodium: String
   },
-  reviews: String,
+  reviews: Number,
   url: String,
   source: String,
-  hash: { type: String, required: true, index: true }
+  hash: {
+    type: String,
+    required: true,
+    index: true
+  }
 });
 
 module.exports = Recipe = mongoose.model("recipe", recipeSchema);
