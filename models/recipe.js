9cf824c1a331e96c0b7f28b9c4b6ccb4ced356ae
Hein Dauven <heindauven@gmail.com>
2019-01-08 15:33:33 +0100

    Add recipe model

---
+++ models/recipe.js
@@ -0,0 +1,28 @@
+const mongoose = rquire("mongoose");
+
+const recipeSchema = new mongoose.Schema({
+  recipeId: { type: Number, required: true },
+  name: { type: String, required: true },
+  categories: { type: [String], index: true },
+  recipeBy: String,
+  imageUrl: String,
+  rating: String,
+  time: { prep: String, cook: String, readyIn: String },
+  servings: String,
+  ingredients: [String],
+  instructions: [String],
+  nutrition: {
+    calories: String,
+    fat: String,
+    carbs: String,
+    proteins: String,
+    cholesterol: String,
+    sodium: String
+  },
+  reviews: String,
+  url: String,
+  source: String,
+  hash: { type: String, required: true, index: true }
+});
+
+module.exports = Recipe = mongoose.model("recipe", recipeSchema);
