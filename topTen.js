0e64b6750e698c203f56d64420c4c91a06e4a9a9
Hein Dauven <heindauven@gmail.com>
2019-06-05 13:14:07 +0200

    Rewrite existing code to work async. Retrieve full recipe data for each category and write the result to disk.

--- topTen.js
+++ topTen.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const fs = require('fs');
 
 const dbName = "recipes";
 const {
@@ -9,109 +10,139 @@ const {
 const MONGODB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/recipes?retryWrites=true`;
 
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
-const client = new MongoClient(MONGODB_URI, {
-  useNewUrlParser: true
-});
-
-client.connect(err => {
-  const db = client.db(dbName);
-
-  topTenPerCategory(db, () => {
-    client.close();
+(async () => {
+  // start here
+  const client = new MongoClient(MONGODB_URI, {
+    useNewUrlParser: true
   });
-});
+  const connection = await client.connect();
+  const collection = await connection.db(dbName).collection('recipes');
+
+  const categories = await topTenPerCategory(collection);
+  const recipes = await retrieveRecipesForCategories(collection, categories);
+
+  fs.writeFile('topTenRecipes.json', JSON.stringify(recipes), (err) => {
+    if (err) throw err;
+    console.log('Saved file!');
+  });
+
+  await client.close();
+})();
 
 let counter = 0;
 
-function topTenPerCategory(db, callback) {
-  const collection = db.collection('recipes');
+async function topTenPerCategory(collection) {
+  return new Promise((resolve, reject) => {
+    const topTens = [];
 
-  collection.find({
-    "categories.0": {
-      "$exists": true
-    },
-    "rating": {
-      "$ne": 0
-    },
-    "reviews": {
-      "$gte": 20
-    }
-  }).count((err, categories) => {
-    console.log(categories)
-  })
+    collection.find({
+      "categories.0": {
+        "$exists": true
+      },
+      "rating": {
+        "$ne": 0
+      },
+      "reviews": {
+        "$gte": 20
+      }
+    }).count((err, categories) => {
+      console.log(categories)
+    })
 
-  collection.aggregate([{
-      "$match": {
-        "categories.0": {
-          "$exists": true
-        },
-        "rating": {
-          "$ne": 0
-        },
-        "reviews": {
-          "$gte": 10
+    collection.aggregate([{
+        "$match": {
+          "categories.0": {
+            "$exists": true
+          },
+          "rating": {
+            "$ne": 0
+          },
+          "reviews": {
+            "$gte": 10
+          }
         }
-      }
-    },
-    {
-      "$project": {
-        "_id": 1,
-        "categories": 1,
-        "rating": 1,
-        "reviews": 1,
-        "name": 1
-      }
-    },
-    {
-      "$unwind": "$categories"
-    },
-    {
-      "$sort": {
-        "rating": -1,
-        "reviews": -1
-      }
-    },
-    {
-      "$group": {
-        "_id": "$categories",
-        "nrOfRecipes": {
-          "$sum": 1
-        },
-        "recipes": {
-          "$push": {
-            "id": "$_id",
-            "rating": "$rating",
-            "reviews": "$reviews",
-            "name": "$name"
+      },
+      {
+        "$project": {
+          "_id": 1,
+          "categories": 1,
+          "rating": 1,
+          "reviews": 1,
+          "name": 1
+        }
+      },
+      {
+        "$unwind": "$categories"
+      },
+      {
+        "$sort": {
+          "rating": -1,
+          "reviews": -1
+        }
+      },
+      {
+        "$group": {
+          "_id": "$categories",
+          "nrOfRecipes": {
+            "$sum": 1
+          },
+          "recipes": {
+            "$push": {
+              "id": "$_id",
+              "rating": "$rating",
+              "reviews": "$reviews",
+              "name": "$name"
+            }
+          }
+        }
+      },
+      {
+        "$match": {
+          "nrOfRecipes": {
+            "$gte": 10
+          }
+        }
+      }, {
+        "$project": {
+          "_id": 1,
+          "nrOfLowReviewCount": 1,
+          "recipes": {
+            "$slice": ["$recipes", 10]
           }
         }
       }
-    },
-    {
-      "$match": {
-        "nrOfRecipes": {
-          "$gte": 10
-        }
-      }
-    }, {
-      "$project": {
-        "_id": 1,
-        "nrOfLowReviewCount": 1,
-        "recipes": {
-          "$slice": ["$recipes", 10]
-        }
-      }
-    }
-  ], (err, cursor) => {
-    cursor.forEach((val, index) => {
-      counter += 1;
-      console.log(counter, val);
-      console.log();
-    }).then(() => {
-      callback(null);
-    }).catch((err) => {
-      console.error(err);
+    ], (err, cursor) => {
+      cursor.forEach((val, index) => {
+        counter += 1;
+        //console.log(counter, val);
+        topTens.push(val);
+      }).then(() => {
+        resolve(topTens);
+      }).catch((err) => {
+        reject(err);
+      });
     });
-  })
+  });
+}
+
+async function retrieveRecipesForCategories(collection, categories) {
+  const topTenRecipes = []
+  for (let category of categories) {
+    let categoryObject = {
+      name: category._id
+    };
+    let recipeArray = []
+
+    for (let recipe of category.recipes) {
+      const recipeData = await collection.findOne({
+        _id: new ObjectID(recipe.id)
+      });
+      recipeArray.push(recipeData);
+    };
+    categoryObject.recipes = recipeArray;
+    topTenRecipes.push(categoryObject);
+  };
+  return topTenRecipes;
 }
